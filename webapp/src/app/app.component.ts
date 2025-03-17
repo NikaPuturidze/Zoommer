847ab5b5f915872e20b00c41d8e2dc21806e4318
Nika Puturidze <nikushaputuridze@gmail.com>
2025-03-17 09:42:44 +0400

    Finished footer visually

--- webapp/src/app/app.component.ts
+++ webapp/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core'
 import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
+import { MainComponent } from './main/main.component'
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
