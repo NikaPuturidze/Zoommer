50b75fc3827875a8f5aa252c9e37e29510ce4481
Nika Puturidze <nikushaputuridze@gmail.com>
2025-03-25 03:02:36 +0400

    Fix: Resovled dependency version conflict

--- webapp/src/app/app.routes.ts
+++ webapp/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router'
 import { MainComponent } from './main/main.component'
+import { ProductDetailsComponent } from './product-details/product-details.component'
 export const routes: Routes = [
   {
     path: '',
     component: MainComponent,
+    title: 'Zoommer.ge',
+  },
+  {
+    path: 'products/:productCategory/:subCategory/:id',
+    component: ProductDetailsComponent,
     title: 'Zoommer.ge',
   },
 ]
